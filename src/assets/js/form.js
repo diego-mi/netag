3cea161919dcb88870d40b14d5fd0c014a40e829
Diego Mi Campos <diegomister@gmail.com>
2018-04-01 17:57:44 -0300

    Refatorado form simplificado

--- src/assets/js/form.js
+++ src/assets/js/form.js
@@ -1,20 +1,30 @@
+/*
+ * Como usar a funcao 'Formulario Simplificado'
+ *
+ * utilize o checkbox simplificado ou outro com a classe, no checkbox, .checkbox-cadastro-simplificado
+ * e
+ * a classe .input-simplified na tag que contem os elementos que devem ser escondidos ou mostrados ao
+ * alterar a forma de cadastro.
+ *
+ */
 var CadastroSimplificado = {
   toggle: function () {
-    console.info(CadastroSimplificado.isSimplified());
     CadastroSimplificado.isSimplified() ? CadastroSimplificado.setAsSimple() : CadastroSimplificado.setAsDefault();
   },
   isSimplified: function () {
     return !!$(".checkbox-cadastro-simplificado").is(":checked");
   },
   setAsSimple: function () {
-    console.info('setAsSimple');
-    console.info($(".input-simplified").length);
-    $(".input-simplified").show();
+    var inputSimplifiedContainer = $(".input-simplified");
+
+    inputSimplifiedContainer.show();
+    inputSimplifiedContainer.find('input').prop("disabled", false);
   },
   setAsDefault: function () {
-    console.info('setAsDefault');
-    console.info($(".input-simplified").length);
-    $(".input-simplified").hide();
+    var inputSimplifiedContainer = $(".input-simplified");
+
+    inputSimplifiedContainer.hide();
+    inputSimplifiedContainer.find('input').prop("disabled", true);
   }
 }
 
